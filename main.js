059cb3a9e5b12d84cb2a72c480c5ccd24c8482a4
minjun Kim <gofiction95@gmail.com>
2022-03-14 11:02:59 +0900

    commit

--- main.js
+++ main.js
@@ -1,7 +1,63 @@
-//숫자 넣고 Go누르면 up인지 down 인지 표시
-//숫자 맞추면 go!!버튼 disabled표시하고 정답입니다. 남은기회 창 부분에
-//go!누르면 남은기회 1차감
-//남은 기회 다 사라지면 Go!버튼 비활성화
-//1부터 100사이 숫자가 아니면 다시 입력하라는 글자 나오게
-//리셋 다시 누르면 죽기 싫다면 맞춰라, 남은 기회 5로 다시 복구
-//같은 숫자입력하고 Go!누르면 이미 입력한 숫자입니다. 다른 숫자를 입력해주세요.
+//랜덤번호 지정
+//유저가 번호를 입력한다
+//만약에 유저가 랜덤번호를 맞추면, 맞췄습니다!
+//랜덤번호가 < 유저번호 DOWN!!!!!!!!
+//랜덤번호가 > 유저번호 UP!!!!!!!!
+//Reset버튼을 누르면 게임이 리셋된다
+//5번의 기회를 다쓰면 게임이 끝난다 (더이상 추측 불가, 버튼이 disable)
+//유저가 이미 입력한 숫자를 또 입력하면, 알려준다, 기회를 깍지 않는다.
+
+let computerNumber = 0;
+let resultArea = document.getElementById("result-Area");
+let chanceArea = document.getElementById("chance-Area");
+let userInput = document.getElementById("user-Input");
+let playButton = document.getElementById("play-Button");
+let resetButton = document.getElementById("reset-Button");
+let chances = 5;
+let history = [];
+
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus",function(){
+  userInput.value = ''
+})
+
+function RandomPick() {
+  computerNumber = Math.floor(Math.random() * 100 + 1);
+  console.log(computerNumber);
+}
+function play() {
+  let userValue = userInput.value;
+  if (history.includes((userValue))) {
+    resultArea.textContent = "같은 수를 입력해서 좀비가 경고합니다!";
+    
+  }else{
+  chances--;
+  chanceArea.textContent = `남은 기회 : ${chances}`;
+  if (userValue == computerNumber) {
+    resultArea.textContent = `맞췄습니다!`;
+    playButton.disabled = true;
+    return;
+  }
+  if (chances == 0) {
+    playButton.disabled = true;
+    return;
+  }
+  if (userValue > computerNumber) {
+    resultArea.textContent = "DOWN!"
+    history.push(userValue)
+    console.log(history)
+  } else if (userValue < computerNumber) {
+    resultArea.textContent = "DOWN!"
+    history.push(userValue)
+    console.log(history)
+  }
+}
+}
+function reset(){
+    chances = 5;
+    resultArea.textContent = '결과가 나온다'
+    chanceArea.textContent = `남은 기회 : ${chances}`;
+    playButton.disabled = false;
+}
+RandomPick();
